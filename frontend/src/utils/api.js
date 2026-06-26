8ebeba3018407ca904afb4c704e3e202f07f5468
UPPALAPATI GEYA MALINI <uppalapatigeyamalini@gmail.com>
2026-06-27 01:08:19 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+
 const API = axios.create({
-  baseURL: "https://budget-tracker-z6gi.onrender.com/api",
+  baseURL: typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
+    ? 'http://localhost:5000/api'
+    : 'https://budget-tracker-z6gi.onrender.com/api',
 });
 
 // Set Authorization header dynamically
