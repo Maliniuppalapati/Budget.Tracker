4609aad20bd8e0efb4266818252770f98c327878
UPPALAPATI GEYA MALINI <uppalapatigeyamalini@gmail.com>
2026-06-27 01:08:54 +0530

    Update API base URL for local and production environments

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 
+ 
 const API = axios.create({
   baseURL: typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:5000/api'
     : 'https://budget-tracker-z6gi.onrender.com/api',
 });
-
 // Set Authorization header dynamically
 export const setAuthToken = (token) => {
   if (token) {
